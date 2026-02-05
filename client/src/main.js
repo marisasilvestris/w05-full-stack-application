14be051780890f5cf30515749074e4e0f4c965e7
Marisa Silvestris <marisasilvestris@gmail.com>
2026-02-05 15:47:47 +0000

    url pupdate

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("app");
 const form = document.getElementById("form");
-const baseURL = "http://localhost:3000";
+const baseURL = "https://w05-full-stack-application.onrender.com/";
 
 async function fetchListings() {
   let pathname = window.location.pathname;
